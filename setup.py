cbb5d1815db7f30af315c422f47c8ad374ea3dca
Rodrigo Araya <rodrigo.araya@gmail.com>
2020-01-30 14:13:08 -0800

    bump marquez-python -> 0.5.7
    Signed-off-by: Rodrigo Araya <rodrigo.araya@gmail.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     url="https://github.com/MarquezProject/marquez-airflow",
     packages=setuptools.find_packages(),
     install_requires=[
-        "marquez-python==0.5.6",
+        "marquez-python==0.5.7",
         "sqlparse==0.2.3"
     ],
 )
